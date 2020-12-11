0eb029b12c27121715b033681f1d2c8deff59146
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-12-11 22:28:13 +0100

    Added login route

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,5 +2,6 @@ const router = require ("express").Router();
 const UserController = require("../controllers/UserController");
 
 router.post('/user/register', UserController.Register);
+router.post('/user/login', UserController.Register);
 
 module.exports = router;
