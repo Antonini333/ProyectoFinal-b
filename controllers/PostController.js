0e7168f6330b5d697f857bd2f20febe395c15cc3
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-12-15 00:02:51 +0100

    Finished Posts CRUD

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -16,7 +16,7 @@ const PostController = {
         } catch (error) {
             console.error(error);
             res.status(500).send({
-                message: 'Post error',
+                message: 'There was a problem posting',
                 error
             })
         }
@@ -35,6 +35,53 @@ catch{
       message: 'There was a problem showing your posts.'
     })
 }
+    },
+
+    async Update(req, res) {
+        try {
+            let updatePost = await PostModel.findOneAndUpdate({
+                _id: req.body._id
+            }, req.body, {
+                new: true
+            });
+            res.send(updatePost);
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({
+                message: 'Something went wrong updating the post'
+            });
+        }
+    },
+
+    async Delete(req, res) {
+        try {
+            let deletePost = await PostModel.findOneAndDelete({
+                _id: req.body._id
+            });
+            res.send({
+                message: "Post successfully deleted",
+                deletePost
+            });
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({
+                message: 'Something went wrong deleting the post'
+            });
+        }
+    },
+
+    async ReadAll(req, res) {
+        try {
+            const allPosts = await PostModel.find();
+            res.send(allPosts);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({
+                message: 'Something went wrong collecting the posts'
+            })
+        }
     }
 
 }
--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -7,7 +7,10 @@ const admin = require("../middleware/admin");
 
 
 router.post('/post', PostController.Create);
-router.get('/readPost', PostController.Read);
+router.get('/readposts', PostController.Read);
+router.put('/updatepost', PostController.Update);
+router.delete('/deletepost', PostController.Delete);
 
+router.get('/readallposts', PostController.ReadAll);
 
 module.exports = router;
