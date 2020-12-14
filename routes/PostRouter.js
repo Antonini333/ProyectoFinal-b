9511ae2a3ad2ca40db9c7dea97c658a829db7559
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-12-14 21:40:03 +0100

    Created Post routes

---
+++ routes/PostRouter.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const PostController = require("../controllers/PostController");
+
+/* MIDDLEWARE */
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+
+
+router.post('/post/create', PostController.Create);
